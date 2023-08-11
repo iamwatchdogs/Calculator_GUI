c8277430c3877b7c62ba77f85aa567055ec094f5
iamwatchdogs <shamith301102@gmail.com>
2023-08-12 00:43:09 +0530

    Added a bit of documentation

--- src/calculator/CalculatorApplication.java
+++ src/calculator/CalculatorApplication.java
@@ -1,9 +1,16 @@
+/**
+ * @author Shamith 
+ * @version 0.0.0
+ * 
+ * This is an simple application program that uses AWT package.
+ * 
+ */
+
 package calculator;
 
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
